1ccc8d41ceb267c3ba5ac4bd3806f342339cdc20
Pichai Tangtrongsakundee <pichai.t@gmail.come>
2021-01-25 00:02:23 +1100

    24-Jan-21

--- TestRunner.groovy
+++ TestRunner.groovy
@@ -1,8 +1,8 @@
 
 def Test01Runner() {
-    def TestA = load "src/TestClass01.groovy"
-    println ('2 About to call TestClass01.testMethod1()!!! ')
-    TestA.testMethod1()
+//    def TestA = load "src/TestClass01.groovy"
+//    println ('2 About to call TestClass01.testMethod1()!!! ')
+//    TestA.testMethod1()
 
     def CC = load "src/Constants.groovy"
     println ('XX1 About to call CC.printPara()!! ')
