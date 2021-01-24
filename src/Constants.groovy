818c9887ae6161c14de5a997723113901d807ce1
Pichai Tangtrongsakundee <pichai.t@gmail.come>
2021-01-24 23:58:42 +1100

    24-Jan-21

--- src/Constants.groovy
+++ src/Constants.groovy
@@ -3,9 +3,9 @@ class CConstants {
     static String TESTPARAMETER = "PICHAI YOU ARE AWESOME!"
 
     def printPara () {
-
-        println('4' + new CConstants().TESTPARAMETER)
-
+        println("TEST ME XXX")
+        println("4" + new CConstants().TESTPARAMETER )
+        println("TEST ME YYY")
     }
 
 }
