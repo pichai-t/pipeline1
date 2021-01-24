fd002133324c5fc70dc07df16ddaac1d081b0c8e
Pichai Tangtrongsakundee <pichai.t@gmail.come>
2021-01-24 23:33:33 +1100

    24-Jan-21

--- TestRunner.groovy
+++ TestRunner.groovy
@@ -4,7 +4,7 @@ import groovy.lang.GroovyClassLoader
 def Test01Runner() {
     def TestA = load "src/TestClass01.groovy"
     println ('2 About to call TestClass01.testMethod1()!!! ')
-    new TestA.testMethod1()
+    TestA.testMethod1()
 }
 
 return this
